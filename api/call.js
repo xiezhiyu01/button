395c7a8e9013cf2d2fbb203dbd010cde822b177f
Zhiyu Xie <zhiyuxie@zhiyus-air.mynetworksettings.com>
2025-08-03 01:54:37 -0400

    fix table name

--- api/call.js
+++ api/call.js
@@ -29,7 +29,7 @@ export default async function handler(req, res) {
     }
 
     const { data, error: readError } = await supabase
-      .from('calls')
+      .from('Calls_Boring')
       .select('count')
       .eq('name', name)
       .single();
